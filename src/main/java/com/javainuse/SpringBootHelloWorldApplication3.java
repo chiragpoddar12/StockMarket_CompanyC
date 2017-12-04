61fcbd123f0d8b9a5b860479fb06248505d2c965
chiragpoddar12 <chiragpoddar12@gmail.com>
2017-12-04 20:52:15 +0000

    Project Upload

---
+++ src/main/java/com/javainuse/SpringBootHelloWorldApplication3.java
@@ -0,0 +1,13 @@
+package com.javainuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringBootHelloWorldApplication3 {
+public static void main(String[] args) {
+		SpringApplication.run(SpringBootHelloWorldApplication3.class, args);
+	}
+}
